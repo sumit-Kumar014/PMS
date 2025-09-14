550e06d85eb4a9fe016fbee17fb7e2e84b26e14b
sumit kumar <sumitkumariit000@gmail.com>
2025-09-14 19:56:29 +0530

    yes-2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,9 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/', // use './' if hosting on a subpath
-  plugins: [
-    tsconfigPaths(), // resolves tsconfig paths
-    tailwindcss() // TailwindCSS integration
-  ],
-  build: {
-    sourcemap: false, // disable sourcemaps in production to avoid errors
-  },
-  resolve: {
-    alias: {
-      // optional, if you want custom aliases
-      '@components': '/src/components',
-      '@utils': '/src/utils',
-      '@routes': '/src/routes',
-    },
-  },
-  server: {
-    port: 5173, // default Vite port
-    open: true, // automatically open in browser
-  },
+  base: '/',
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
