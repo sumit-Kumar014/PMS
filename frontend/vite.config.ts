ff754343f4dd126d6d2725b13f134c7034de60e3
sumit kumar <sumitkumariit000@gmail.com>
2025-09-14 22:14:13 +0530

    bug-fix-3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   base: '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    sourcemap: false,
+  },
 });
